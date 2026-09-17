bba61d326fb86aa5e05c29d9456387a729c67cf9
Rioreyblue <franciscorey702@gmail.com>
2026-09-17 11:37:05 +0800

    adding structures set up

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+structure
+ - __init__.py for python import pakage make it easier for file functions
+ - con
--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title= "Rey System \"Generate Your Adventure Game Api\"",
+    description="Generate your adventure",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+
