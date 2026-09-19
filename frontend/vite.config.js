a28703675803db9550b30857659f10dd4c402be5
Rioreyblue <franciscorey702@gmail.com>
2026-09-19 21:25:16 +0800

    lastly connect backend api into frontend at vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://0.0.0.0:8000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
